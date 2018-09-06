3cf784f0e5b18e5b8740dd64b9314e19a6295468
Matt <Matt@10.0.0.49>
2018-09-05 22:22:15 -0500

    Added javadoc

--- src/main/java/mudmap2/utils/KeystrokeHelper.java
+++ src/main/java/mudmap2/utils/KeystrokeHelper.java
@@ -6,10 +6,22 @@ import javax.swing.KeyStroke;
 
 public class KeystrokeHelper {
 
+    /**
+     * Returns a KeyStroke for the given keyCode using the CTRL key modifier
+     *
+     * @param keyCode an int specifying the numeric code for a keyboard key
+     * @return
+     */
     public static KeyStroke ctrl(final int keyCode) {
         return KeyStroke.getKeyStroke(keyCode, ActionEvent.CTRL_MASK);
     }
 
+    /**
+     * Returns a KeyStroke for the given keyCode using the CTRL ALT key modifier
+     *
+     * @param keyCode an int specifying the numeric code for a keyboard key
+     * @return
+     */
     public static KeyStroke ctrlAlt(final int keyCode) {
         return KeyStroke.getKeyStroke(keyCode, ActionEvent.CTRL_MASK + ActionEvent.ALT_MASK);
     }
